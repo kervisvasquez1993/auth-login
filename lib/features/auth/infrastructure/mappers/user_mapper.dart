2725df4f5ba01c43afe77ec13ae5ecf45b203bc4
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-04-23 00:36:05 -0400

    create use cases and mappers

--- lib/features/auth/infrastructure/mappers/user_mapper.dart
+++ lib/features/auth/infrastructure/mappers/user_mapper.dart
@@ -1,13 +1,10 @@
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 
 class UserMapper {
-  static User userJsonToEntity(Map<String, dynamic> json) {
-    return User(
+  static User userJsonToEntity(Map<String, dynamic> json) => User(
       id: json['id'],
-      fullName: json['fullName'],
       email: json['email'],
-      token: json['token'],
+      fullName: json['fullName'],
       roles: List<String>.from(json['roles'].map((role) => role)),
-    );
-  }
+      token: json['token'] ?? '');
 }
