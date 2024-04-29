e9d6d7143156aac23b2f53d8022257f4584a6af3
kervisvasquez1993 <kervisvasquez24@gmail.com>
2024-04-29 02:53:28 -0400

    update img in product in backend

--- lib/features/products/infrastructure/datasources/products_datasources_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasources_impl.dart
@@ -12,6 +12,28 @@ class ProductDatasourceImpl implements ProductsDatasource {
       : dio = Dio(BaseOptions(
             baseUrl: Enviroment.apiUrl,
             headers: {'Authorization': 'Bearer $accesToken'}));
+  Future<String> _uploadFile(String path) async {
+    try {
+      final fileName = path.split('/').last;
+      final FormData data = FormData.fromMap(
+          {'file': await MultipartFile.fromFile(path, filename: fileName)});
+      final response = await dio.post('/files/product', data: data);
+      return response.data['image'];
+    } catch (e) {
+      throw Exception();
+    }
+  }
+
+  Future<List<String>> _uploadPhotos(List<String> photos) async {
+    final photosToUpload =
+        photos.where((element) => element.contains('/')).toList();
+    final photosToIgnore =
+        photos.where((element) => !element.contains('/')).toList();
+    final List<Future<String>> uploadJob =
+        photosToUpload.map(_uploadFile).toList();
+    final newImages = await Future.wait(uploadJob);
+    return [...photosToIgnore, ...newImages];
+  }
 
   @override
   Future<Product> createUpdateProduct(Map<String, dynamic> productLike) async {
@@ -21,6 +43,7 @@ class ProductDatasourceImpl implements ProductsDatasource {
       final String url =
           (productId == null) ? '/products' : '/products/$productId';
       productLike.remove('id');
+      productLike['images'] = await _uploadPhotos(productLike['images']);
       final response = await dio.request(url,
           data: productLike, options: Options(method: method));
 
